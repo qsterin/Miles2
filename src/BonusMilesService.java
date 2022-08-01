7db618fa676f1377f33de59740a3dac7d75b34a9
 Eвгений Костерин <qsterin@gmail.com>
2022-08-01 12:25:34 +0300

    reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,10 @@
 public class BonusMilesService {
     int oneMile = 20;
+
     public int calculate(int ticketPrice) {
-        int miles = ticketPrice/oneMile;
+        int miles = ticketPrice / oneMile;
         return miles;
     }
 
-    }
+}
 
